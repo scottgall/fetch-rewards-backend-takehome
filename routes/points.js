3b96c43b3685bb9d122c87db5e461aac5d129fcb
Scott Gall <scottarthurgall@gmail.com>
2021-03-15 16:46:15 -0500

    add points working

--- routes/points.js
+++ routes/points.js
@@ -3,17 +3,8 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
 let totalPoints = 0;
-let payerPoints = {
-  "UNILEVER": 0,
-  "MILLER COORS": 0
-};
+let payerPoints = {};
 let pointTransactions = [];
-
-let transactionKeys = ['payer', 'points', 'timestamp']
-function validateKeys(obj) {
-  return transactionKeys.every({}.hasOwnProperty.bind(obj));
-};
-
 
 // all routes in here are starting with /users
 router.get('/', (req, res) => {
@@ -35,6 +26,7 @@ router.post('/', [
 }, (req, res) => {
     let payer = req.body.payer
     let points = req.body.points
+
     if ((!payerPoints[payer] && points < 0) || ((payerPoints[payer] + points) < 0)) {
       return res.status(422).json({ error: "Unable to add transaction", reason: "Payer balance can't go negative"});
     } else if (!payerPoints[payer]) {
@@ -42,14 +34,48 @@ router.post('/', [
     } else {
       payerPoints[payer] += points
     }
-    pointTransactions.push(req.body)
-    for (i=0;i<pointTransactions.length;i++) {
-      console.log(pointTransactions[i]);
-    };
-    while points >= 0
-    console.log(payerPoints)
-    console.log('----------------------')
+
+    totalPoints += points
+    
+    if (points < 0) {
+      absPoints = Math.abs(points);
+      console.log(absPoints)
+      removeIndex = [];
+
+      var counter = pointTransactions.length - 1;
+      while (absPoints > 0) {
+        if (pointTransactions[counter]['payer'] === payer) {
+          if (pointTransactions[counter]['points'] === absPoints) {
+            absPoints = 0
+            removeIndex.push(counter)
+          } else if (pointTransactions[counter]['points'] > absPoints) {
+            pointTransactions[counter]['points'] -= absPoints
+            absPoints = 0
+          } else if (pointTransactions[counter]['points'] < absPoints) {
+            removeIndex.push(counter)
+            absPoints -= pointTransactions[counter]['points']
+          }
+        }
+        counter--
+      }
+      for (i=0;i<removeIndex.length;i++) {
+        pointTransactions.splice(removeIndex[i],1);
+      }
+    }
+
+    if (points > 0) {
+      pointTransactions.push(req.body);
+      pointTransactions = pointTransactions.sort((a,b) => b.timestamp - a.timestamp);
+    }
+    // for (i=0;i<pointTransactions.length;i++) {
+    //   console.log(pointTransactions[i]);
+    // };      
+    // console.log(payerPoints)
+    // console.log(`total ${totalPoints}`)
+    // console.log('----------------------')
     res.status(200).json({ success: "transaction added", transaction: req.body})
 });
+
+
 
 module.exports = router;
